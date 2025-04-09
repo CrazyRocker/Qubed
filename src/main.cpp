c50e9811c70058b36f1549077de1eda17b78deb6
Panshul Acharya <67586421+CrazyRocker@users.noreply.github.com>
2025-04-09 17:04:05 +0530

    Made some changes to main.cpp to remove an unnecessary include (stdexcept), and also made some stuff about the glfwVector more accurate and quicker.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
-#include <stdexcept>
 #include <vulkan/vulkan.hpp>
 #define GLFW_INCLUDE_VULKAN //MUST define this before including GLFW/glfw3.h so it knows to use Vulkan
 #include <GLFW/glfw3.h>
 
 //These aren't modified ever in the program, so I've declared as constant. Feel free to change this as needed
-const uint32_t WIDTH = 800;
-const uint32_t HEIGHT = 600;
-const char NAME[] = "Vulkan App Test";
+constexpr uint32_t WIDTH = 800;
+constexpr uint32_t HEIGHT = 600;
+constexpr char NAME[] = "Vulkan App Test";
 
 void key_callback(GLFWwindow* window,int key, int scanCode, int action, int mods){
     if(key==GLFW_KEY_ESCAPE && action==GLFW_PRESS)
@@ -18,43 +17,61 @@ static VKAPI_ATTR vk::Bool32 VKAPI_CALL debugCallback(vk::DebugUtilsMessageSever
     vk::DebugUtilsMessageTypeFlagsEXT messageType,
     const vk::DebugUtilsMessengerCallbackDataEXT* pCallbackData,
     void* pUserData) {
-        std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+        std::cout << "validation layer: " << pCallbackData->pMessage << std::endl;
         return VK_FALSE;
 }
 
 int main() {
     
-    if(!glfwInit())
-        throw std::runtime_error("Error while initializing GLFW!");
+    if(!glfwInit()){
+        std::cout << "Error while initializing GLFW!" << std::endl;
+        return -1;
+    }
+
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
     
     GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, NAME, nullptr, nullptr);
-    if(!window)
-        throw std::runtime_error("Failed to create GLFW window!");
+    if(!window){
+        std::cout<<"Failed to create GLFW window!"<<std::endl;
+        return -1;
+    }
 
     glfwSetKeyCallback(window, key_callback);
 
     vk::ApplicationInfo appInfo(NAME, VK_MAKE_VERSION(0, 1, 0), "No Engine",
                                 VK_MAKE_VERSION(0, 1, 0), VK_API_VERSION_1_4);
     
+
     uint32_t glfwExtensionCount =0;
-    const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-    std::vector<const char*> glfwExtensionsVector(glfwExtensions, glfwExtensions + glfwExtensionCount);
-    glfwExtensionsVector.push_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+    const char** instanceExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    std::vector<const char*> instanceExtensionsVector;
+    instanceExtensionsVector.reserve(glfwExtensionCount + 1 +   //The +1 here reserves space for the portability extension
     #ifndef NDEBUG
-    glfwExtensionsVector.push_back("VK_EXT_debug_utils");
-    std::vector<const char*> layers = std::vector<const char*>{"VK_LAYER_KHRONOS_validation"};
+        1       //If debug is enabled, we want to reserve one more space for the VK_EXT_debug_utils extension
     #endif
+    );
+    for(int i=0; i<glfwExtensionCount; i++){
+        instanceExtensionsVector.push_back(instanceExtensions[i]);
+        
+    }
+    instanceExtensionsVector.push_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+    
+    #ifndef NDEBUG
+    instanceExtensionsVector.push_back("VK_EXT_debug_utils");
+    const char* layers[] = {"VK_LAYER_KHRONOS_validation"};
+    #endif
+    
     
     auto instance = vk::createInstanceUnique(vk::InstanceCreateInfo{
         vk::InstanceCreateFlags{ VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR }, &appInfo,
         #ifndef NDEBUG
-        static_cast<uint32_t>(layers.size()), layers.data(),
+        static_cast<uint32_t>(sizeof(layers)/sizeof(layers[1])), layers,
         #else
         0, nullptr,
         #endif
-        static_cast<uint32_t>(glfwExtensionsVector.size()), glfwExtensionsVector.data()
+        static_cast<uint32_t>(instanceExtensionsVector.size()), instanceExtensionsVector.data()
     });
 
     vk::detail::DispatchLoaderDynamic dld(instance.get(), vkGetInstanceProcAddr);
@@ -69,8 +86,10 @@ int main() {
     nullptr, dld);
     
     VkSurfaceKHR surfaceTemp;
-    if(VK_SUCCESS != glfwCreateWindowSurface(*instance, window, nullptr, &surfaceTemp))
-        throw std::runtime_error("Failed to create a Window Surface in GLFW");
+    if(VK_SUCCESS != glfwCreateWindowSurface(*instance, window, nullptr, &surfaceTemp)) {
+        std::cout << "Failed to create a Window Surface in GLFW" << std::endl;
+        return -1;
+    }
 
     vk::UniqueSurfaceKHR surface(surfaceTemp, *instance);
 
@@ -101,13 +120,18 @@ int main() {
     }
 
     if (graphicsQueueFamilyIndex == UINT32_MAX) {
-        if(presentQueueFamilyIndex == UINT32_MAX)
-            throw std::runtime_error("Couldn't find a suitable graphics queue family and a present queue family");
-        else
-            throw std::runtime_error("Couldn't find a suitable graphics queue family!");
+        if(presentQueueFamilyIndex == UINT32_MAX) {
+            std::cout << "Couldn't find a suitable graphics queue family and a present queue family" << std::endl;
+            return -1;
+        } else {
+            std::cout << "Couldn't find a suitable graphics queue family!" << std::endl;
+            return -1;
+        }
     } else {
-        if(presentQueueFamilyIndex == UINT32_MAX)
-            throw std::runtime_error("Couldn't find a suitable present Queue family");
+        if(presentQueueFamilyIndex == UINT32_MAX) {
+            std::cout << "Couldn't find a suitable present Queue family" << std::endl;
+            return -1;
+        }
     }
 
     std::cout << "Graphics Queue Family Index: " << graphicsQueueFamilyIndex << "\n";
