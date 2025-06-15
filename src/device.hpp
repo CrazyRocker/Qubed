b4de2961af05d9758ae8967169cb421341636fe9
Panshul the Cool Kitty-God :3 (Might bite) <67586421+CrazyRocker@users.noreply.github.com>
2025-06-15 16:03:21 +0530

    Cleaned up device.hpp to remove functions which were, for one reason or another never defined or removed, and also arranged everything in better order.

--- src/device.hpp
+++ src/device.hpp
@@ -14,13 +14,11 @@ constexpr uint8_t requiredExtensionsCount = sizeof(requiredExtensions)/sizeof(re
 
 SuitabilityInfo isPhysicalDeviceSuitable(vk::PhysicalDevice device, vk::UniqueSurfaceKHR& surface);
 
-std::pair<std::optional<uint32_t>, std::optional<uint32_t>> getGraphicsQueueAndPresentQueue_optional(const vk::PhysicalDevice& physicalDevice, const vk::UniqueSurfaceKHR& surface);
-
 vk::PhysicalDevice getPhysicalDevice(vk::UniqueInstance& instance, vk::UniqueSurfaceKHR& surface);
 
-bool isSwapChainViable(const vk::UniqueSurfaceKHR& surface, const vk::PhysicalDevice& device);
+std::pair<std::optional<uint32_t>, std::optional<uint32_t>> getGraphicsQueueAndPresentQueue_optional(const vk::PhysicalDevice& physicalDevice, const vk::UniqueSurfaceKHR& surface);
 
-bool doesDeviceSupportRequiredExtensions(vk::PhysicalDevice physicalDevice);
+std::pair<uint32_t, uint32_t> getGraphicsQueueAndPresentQueue(const vk::PhysicalDevice& physicalDevice, const vk::UniqueSurfaceKHR& surface);
 
 vk::UniqueDevice getUniqueDevice(vk::UniqueInstance& instance, vk::UniqueSurfaceKHR& surface);
 
